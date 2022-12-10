386ee9efa656af2045b2b90b748542a3fc830c94
nitinbhagat12 <119115476+nitinbhagat12@users.noreply.github.com>
2022-12-10 08:26:17 +0530

    Add files via upload

---
+++ Insurance amount creater.py
@@ -0,0 +1,15 @@
+c=int(input("Enter th price of the car you drive: "))
+a=str(input("Enter your gender: "))
+if a=="Male":
+    b=int(input("Enter ur age: ")
+        if b<=23:
+            print("The insurance amount is ", 0.23*c)
+        elif b>23:
+            print("The insurance amount is ", 0.9*c)
+elif a=="Female":
+    d=str(input("Weather u drive a Sports car or not (y:yes/n:no)")
+        if d=="y":
+            print("The insurance amount is ", 0.21*c)
+        if d=="n":
+            print("The insurance amount is ", 0.1*c)
+
